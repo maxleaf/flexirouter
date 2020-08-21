69747576cbce310ec22085ed29a4549e26989f66
Denys Haryachyy <denysh@flexiwan.com>
2020-08-21 12:48:32 +0300

    Build vppsb using CMake.

--- scripts/vpp_build.sh
+++ scripts/vpp_build.sh
@@ -17,10 +17,12 @@ esac
 done
 echo "CLEAN = ${CLEAN}"
 
+rm vpp/src/plugins/netlink
+rm vpp/src/plugins/router
+ln -sf ../../../vppsb/netlink vpp/src/plugins/netlink
+ln -sf ../../../vppsb/router vpp/src/plugins/router
 
 cd vpp
-
-VPP_PATH=`pwd`
 
 if  [ "${CLEAN}" == "YES" ] ; then
   make wipe
@@ -28,48 +30,3 @@ fi
 make build
 
 cd -
-
-cd vppsb
-cd netlink
-
-sed -i 's/AM_CFLAGS += -O2.*/AM_CFLAGS += -O2 -DCLIB_VEC64=0/g' Makefile.am
-
-libtoolize
-aclocal
-autoconf
-automake --add-missing
-./configure VPP_DIR=$VPP_PATH --enable-debug
-make
-
-# Copy it to /usr/lib/ so loader will find it while loading router.so plugin 
-if [ -d $VPP_PATH/build-root/build-vpp_debug-native/vpp/lib/ ]; then
-  sudo ln -sfn $(pwd)/.libs/librtnl.so $VPP_PATH/build-root/build-vpp_debug-native/vpp/lib/librtnl.so
-  sudo ln -sfn $(pwd)/.libs/librtnl.so.0 /usr/lib/x86_64-linux-gnu/librtnl.so.0
-fi
-if [ -d $VPP_PATH/build-root/build-vpp-native/vpp/lib/ ]; then
-  sudo ln -sfn $(pwd)/.libs/librtnl.so $VPP_PATH/build-root/build-vpp-native/vpp/lib/librtnl.so
-  sudo ln -sfn $(pwd)/.libs/librtnl.so.0 $VPP_PATH/build-root/build-vpp-native/vpp/lib/librtnl.so.0
-fi
-
-cd -
-
-cd router
-
-sed -i 's#AM_CFLAGS = -Wall -I@TOOLKIT_INCLUDE@.*#AM_CFLAGS = -Wall -I@TOOLKIT_INCLUDE@ -DCLIB_DEBUG -DCLIB_VEC64=0 -I../../vpp/build-root/build-vpp_debug-native/vpp -I../../vpp/src -I../netlink#g' Makefile.am
-
-libtoolize
-aclocal
-autoconf
-automake --add-missing
-./configure
-make
-
-if [ -d $VPP_PATH/build-root/build-vpp_debug-native/vpp/lib/ ]; then
-  ln -sfn $(pwd)/.libs/router.so $VPP_PATH/build-root/build-vpp_debug-native/vpp/lib/vpp_plugins/router.so
-fi
-if [ -d $VPP_PATH/build-root/build-vpp-native/vpp/lib/ ]; then
-  ln -sfn $(pwd)/.libs/router.so $VPP_PATH/build-root/build-vpp-native/vpp/lib/vpp_plugins/router.so
-fi
-
-cd $VPP_PATH
-cd ..
