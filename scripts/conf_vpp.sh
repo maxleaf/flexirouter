6c8d9a6ed6f55a93d623db5252a60049b5de7e1d
Denys Haryachyy <denysh@flexiwan.com>
2020-11-30 10:11:36 +0000

    Updated test scripts.

--- scripts/conf_vpp.sh
+++ scripts/conf_vpp.sh
@@ -3,4 +3,8 @@
 SCRIPTS_PATH=`pwd`/scripts
 VPP_PATH=vpp/build-root/build-vpp_debug-native/vpp/bin
 
+cp scripts/50-cloud-init.baseline.yaml /etc/netplan
+
 $VPP_PATH/vppctl exec $SCRIPTS_PATH/init.cli
+
+netplan apply
--- scripts/run_vpp.sh
+++ scripts/run_vpp.sh
@@ -6,7 +6,6 @@ SCRIPTS_PATH=vpp/extras/vpp_config/scripts
 
 ip link set dev enp0s3 down
 ip link set dev enp0s8 down
-ip link set dev enp0s9 down
 
 cd vpp
 
@@ -14,6 +13,8 @@ gdb --args ./build-root/build-vpp_debug-native/vpp/bin/vpp -c ../scripts/startup
 
 cd -
 
-python $SCRIPTS_PATH/dpdk-devbind.py -b e1000 00:03.0 00:08.0 00:09.0
+python $SCRIPTS_PATH/dpdk-devbind.py -b e1000 00:03.0 00:08.0
+
+rm /etc/netplan/50-cloud-init.baseline.yaml
 
 netplan apply
